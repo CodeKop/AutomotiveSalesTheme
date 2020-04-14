bb03f63ce791048340396c7e5d6bec107f0c3ca3
Jesse <jesseb727@gmail.com>
2020-04-14 22:50:50 +0200

    Build

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1193,7 +1193,7 @@ for (var i in _events2['default'].prototype) {
 /* 9 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n      padding-right: 150px;\n                          ^\n      Invalid CSS after \"...g-right: 150px;\": expected selector, was \"''\"\n      in /home/randy/Documents/AutomotiveSalesTheme/_dev/css/theme.scss (line 274, column 28)\n    at runLoaders (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/neo-async/async.js:8067:18)\n    at options.error (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 /* 10 */
