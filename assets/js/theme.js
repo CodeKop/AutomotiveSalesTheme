dc7fe58189587afabef7d06f2799196cf776720d
Jesse <jesseb727@gmail.com>
2020-04-14 13:50:27 +0200

    Build

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2098,38 +2098,40 @@ var Slider = (function () {
             var slider = this.el.find('.slider');
 
             if (phase === "move" && (direction === "left" || direction === "right")) {
-                console.log("Swipe Triggered");
+                // console.log("Swipe Triggered");
                 if (direction === "left") {
-                    slider.animate({
-                        scrollLeft: this.lastScrollLeft - distance
-                    }, 'fast');
+                    slider.scrollLeft(this.lastScrollLeft + distance);
+                    // slider.animate({
+                    //     scrollLeft: this.lastScrollLeft - distance
+                    // }, 'fast');
                 } else if (direction === "right") {
-                    slider.animate({
-                        scrollLeft: this.lastScrollLeft + distance
-                    }, 'fast');
-                }
+                        slider.scrollLeft(this.lastScrollLeft - distance);
+                        // slider.animate({
+                        //     scrollLeft: this.lastScrollLeft + distance
+                        // }, 'fast');
+                    }
             } else if (phase === "cancel") {
-                slider.animate({
-                    scrollLeft: this.lastScrollLeft
-                }, 'fast');
-            } else if (phase === "end") {
-                var nearestItemScroll = slider.scrollLeft,
-                    itemWidth = slider.children('.slider-tem').outerWidth(true),
-                    round = nearestItemScroll % itemWidth,
-                    multiplier = Math.floor(nearestItemScroll / itemWidth);
+                    slider.animate({
+                        scrollLeft: this.lastScrollLeft
+                    }, 'fast');
+                } else if (phase === "end") {
+                    var nearestItemScroll = slider.scrollLeft,
+                        itemWidth = slider.children('.slider-tem').outerWidth(true),
+                        round = nearestItemScroll % itemWidth,
+                        multiplier = Math.floor(nearestItemScroll / itemWidth);
 
-                if (round >= 5) {
-                    nearestItemScroll = (multiplier + 1) * itemWidth;
-                } else {
-                    nearestItemScroll = multiplier * itemWidth;
+                    if (round >= 5) {
+                        nearestItemScroll = (multiplier + 1) * itemWidth;
+                    } else {
+                        nearestItemScroll = multiplier * itemWidth;
+                    }
+
+                    slider.animate({
+                        scrollleft: nearestItemScroll
+                    }, 'fast');
+
+                    this.updateScroll();
                 }
-
-                slider.animate({
-                    scrollleft: nearestItemScroll
-                }, 'fast');
-
-                this.updateScroll();
-            }
         }
     }]);
 
