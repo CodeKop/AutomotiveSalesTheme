b0128273919ab6a5f6d9b13a857ee41b3dddfef3
Jesse <jesseb727@gmail.com>
2020-04-30 16:09:59 +0200

    Build

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2142,6 +2142,7 @@ var TopMenu = (function (_DropDown) {
         var target = (0, _jquery2['default'])(_this),
             targetDepth = 2;
 
+        console.log(target.find('.top-menu[data-depth="' + targetDepth + '"]').length);
         if (target.find('.top-menu[data-depth="' + targetDepth + '"]').length) {
           target.addClass('position-static');
           target.addClass('submenu--full');
