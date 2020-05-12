5625dfc0e10b0f5381c2cad182bcbb5f447e2634
Jesse Boise <jesseb727@gmail.com>
2020-05-12 15:27:00 +0200

    Update swiper options

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -30,57 +30,61 @@ import $ from "jquery";
 
 // "inherit" EventEmitter
 for (var i in EventEmitter.prototype) {
-  prestashop[i] = EventEmitter.prototype[i];
+	prestashop[i] = EventEmitter.prototype[i];
 }
 
 $(document).ready(() => {
-  let dropDownEl = $('.js-dropdown');
-  const form = new Form();
-  let topMenuEl = $('.js-top-menu ul[data-depth="0"]');
+	let dropDownEl = $('.js-dropdown');
+	const form = new Form();
+	let topMenuEl = $('.js-top-menu ul[data-depth="0"]');
 
-  let dropDown = new DropDown(dropDownEl);
-  let topMenu = new TopMenu(topMenuEl);
-  let productMinitature = new ProductMinitature();
-  let productSelect = new ProductSelect();
-  let searchBar = new SearchBar();
+	let dropDown = new DropDown(dropDownEl);
+	let topMenu = new TopMenu(topMenuEl);
+	let productMinitature = new ProductMinitature();
+	let productSelect = new ProductSelect();
+	let searchBar = new SearchBar();
 
-  dropDown.init();
-  form.init();
-  topMenu.init();
-  productMinitature.init();
-  productSelect.init();
-  searchBar.init();
+	dropDown.init();
+	form.init();
+	topMenu.init();
+	productMinitature.init();
+	productSelect.init();
+	searchBar.init();
 
-  if (prestashop.configuration.is_catalog) {
-    $("*[id^='_(desktop|mobile)_cart']").each((idx, el) => {
-      el.remove();
-    });
-  }
+	if (prestashop.configuration.is_catalog) {
+		$("*[id^='_(desktop|mobile)_cart']").each((idx, el) => {
+			el.remove();
+		});
+	}
 
-  var swiper = new Swiper(
-    '.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
+	var swiper = new Swiper('.swiper-container', {
+		navigation: {
+			nextEl: '.swiper-btn-next',
+			prevEl: '.swiper-btn-prev'
+		},
 
-    breakpoints: {
-      576: {
-        slidesPerView: 2
-      },
-      768: {
-        slidesPerView: 4
-      }
-    }
-  });
+		slidesPerView: 1,
+		spaceBetween: 10,
 
-  $('.carousel[data-touch="true"]').swipe({
-    swipe(event, direction, distance, duration, fingerCount, fingerData) {
-      if (direction == 'left') {
-        $(this).carousel('next');
-      }
-      if (direction == 'right') {
-        $(this).carousel('prev');
-      }
-    },
-    allowPageScroll: 'vertical',
-  });
+		breakpoints: {
+			576: {
+				slidesPerView: 2
+			},
+			768: {
+				slidesPerView: 4
+			}
+		}
+	});
+
+	$('.carousel[data-touch="true"]').swipe({
+		swipe(event, direction, distance, duration, fingerCount, fingerData) {
+			if (direction == 'left') {
+				$(this).carousel('next');
+			}
+			if (direction == 'right') {
+				$(this).carousel('prev');
+			}
+		},
+		allowPageScroll: 'vertical',
+	});
 });
