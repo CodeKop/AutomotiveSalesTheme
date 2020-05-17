c79b7eef9236daff5be41352adc57e1db01d3cb8
Jesse Boise <jesseb727@gmail.com>
2020-05-17 15:02:20 +0200

    Build

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1015,7 +1015,7 @@ for (var i in _events2['default'].prototype) {
 /* 9 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n            box-shadow: -3px 3px 0 $inverse;\n                                  ^\n      Undefined variable: \"$inverse\".\n      in /home/randy/Documents/AutomotiveSalesTheme/_dev/css/components/featuredproducts.scss (line 223, column 36)\n    at runLoaders (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/neo-async/async.js:8067:18)\n    at options.error (/home/randy/Documents/AutomotiveSalesTheme/_dev/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ }),
 /* 10 */
