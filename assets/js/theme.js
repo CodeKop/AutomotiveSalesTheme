51d05c38b736d282a02a785d7db33b9b66ee3c10
Jesse <jesseb727@gmail.com>
2020-06-30 20:42:43 +0200

    Build

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1094,7 +1094,7 @@ var featuredSwiperOptions = {
 /* 9 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\nundefined\r\n                   ^\r\n      Invalid CSS after \"...adding-top: map\": expected expression (e.g. 1px, bold), was \".get($spacers, 3);\"\r\n      in C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\css\\components\\brandlist.scss (line 8, column 21)\n    at C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\webpack\\lib\\NormalModule.js:192:19\n    at C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.callback (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\neo-async\\async.js:8067:18)\n    at options.error (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\node-sass\\lib\\index.js:294:32)");
 
 /***/ }),
 /* 10 */
