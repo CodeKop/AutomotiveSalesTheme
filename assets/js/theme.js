22c2d8a172ae0e7676a17632090d15025ad5ab3f
Jesse <jesseb727@gmail.com>
2020-07-12 12:55:19 +0200

    Build

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1095,7 +1095,7 @@ var featuredSwiperOptions = {
 /* 9 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\nundefined\r\n                          ^\r\n      Undefined variable: \"$gray-light-secondary\".\r\n      in C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\css\\components\\manufacturer.scss (line 38, column 28)\n    at C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\webpack\\lib\\NormalModule.js:192:19\n    at C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.callback (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\neo-async\\async.js:8067:18)\n    at options.error (C:\\Users\\jesse\\Projects\\AutomotiveSalesTheme\\_dev\\node_modules\\node-sass\\lib\\index.js:294:32)");
 
 /***/ }),
 /* 10 */
